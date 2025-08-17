8b6c2d3d326ce9274a78040c67fa8747356da814
FeRodAlb <felipe.r.albuquerque@gmail.com>
2025-08-17 19:21:53 -0300

    feat: add String Uppercase Handler

--- basic-function/src/main/java/ferodalb/lambda/string/StringUppercaseHandler.java
+++ basic-function/src/main/java/ferodalb/lambda/string/StringUppercaseHandler.java
@@ -1,15 +1,18 @@
-package ferodalb.lambda;
+package ferodalb.lambda.string;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class StringHandler implements RequestHandler<String, String> {
+public class StringUppercaseHandler implements RequestHandler<String, String> {
 
     @Override
     public String handleRequest(String event, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("EVENT TYPE: " + event.getClass());
+        logger.log("Handling UPPERCASE event for input: " + event);
+        if (event == null || event.trim().isEmpty()) {
+            throw new IllegalArgumentException("Input string must not be null or empty.");
+        }
         return event.toUpperCase();
     }
 }
